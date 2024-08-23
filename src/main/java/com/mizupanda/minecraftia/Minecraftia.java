0e8862c7107da5ac873f2ded66dc44560792f63d
Juniel Djossou <junieldjossou@gmail.com>
2024-08-23 19:07:28 -0400

    Tutorial Items

--- src/main/java/com/mizupanda/minecraftia/Minecraftia.java
+++ src/main/java/com/mizupanda/minecraftia/Minecraftia.java
@@ -1,5 +1,8 @@
 package com.mizupanda.minecraftia;
 
+import com.mizupanda.minecraftia.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -77,6 +80,8 @@ public class Minecraftia
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -100,11 +105,10 @@ public class Minecraftia
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        /*
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
-
-         */
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/mizupanda/minecraftia/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.mizupanda.minecraftia.item;
+
+import com.mizupanda.minecraftia.Minecraftia;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Minecraftia.MOD_ID);
+
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
