766042fb38cf15c3653b138e619b804b7563c6fe
Juniel Djossou <junieldjossou@gmail.com>
2024-09-04 21:43:28 -0400

    Tutorial Custom Tooltips

--- src/main/java/com/mizupanda/minecraftia/item/ModItems.java
+++ src/main/java/com/mizupanda/minecraftia/item/ModItems.java
@@ -3,10 +3,15 @@ package com.mizupanda.minecraftia.item;
 import com.mizupanda.minecraftia.Minecraftia;
 import com.mizupanda.minecraftia.item.custom.ChiselItem;
 import com.mizupanda.minecraftia.item.custom.FuelItem;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Minecraftia.MOD_ID);
@@ -21,7 +26,13 @@ public class ModItems {
             () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static final DeferredItem<Item> RADISH = ITEMS.register("radish",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)) {
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.minecraftia.radish.tooltip"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static final DeferredItem<Item> FROSTFIRE_ICE = ITEMS.register("frostfire_ice",
             () -> new FuelItem(new Item.Properties(), 800));
--- src/main/java/com/mizupanda/minecraftia/item/custom/ChiselItem.java
+++ src/main/java/com/mizupanda/minecraftia/item/custom/ChiselItem.java
@@ -1,17 +1,22 @@
 package com.mizupanda.minecraftia.item.custom;
 
 import com.mizupanda.minecraftia.block.ModBlocks;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChiselItem extends Item {
@@ -51,5 +56,16 @@ public class ChiselItem extends Item {
             }
         }
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        if (Screen.hasShiftDown()) {
+            tooltipComponents.add(Component.translatable("tooltip.minecraftia.chisel.shift_down"));
+        } else {
+            tooltipComponents.add(Component.translatable("tooltip.minecraftia.chisel"));
+        }
+
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
